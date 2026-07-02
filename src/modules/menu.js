bfa3056fb4123d9f34e63081dd0c3616d707be66
UlumDcode <apumm12@gmail.com>
2026-07-02 23:03:54 +0700

    odin restaurant

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,22 +1,28 @@
 // src/modules/menu.js
-const importImg = (r) => r.keys().map(r);
-const menuImages = importImg(
-  require.context("../assets/images/menu", false, /\.(png|jpe?g|svg)$/),
-);
+import menu1 from "../assets/img/menu1.jpg";
+import menu2 from "../assets/img/menu2.jpg";
+import menu3 from "../assets/img/menu3.jpg";
+import menu4 from "../assets/img/menu4.jpg";
+import menu5 from "../assets/img/menu5.jpg";
 
 export function loadMenu(container) {
   const menuDiv = document.createElement("div");
   menuDiv.classList.add("menu-wrapper");
 
+  const menuImages = [menu1, menu2, menu3, menu4, menu5];
+
   menuDiv.innerHTML = `
-  <h1 class="page-title">Menu</h1>
-<div class="menuImage">
-${menuImages.map((imgSrc) => `<img class="menuImage" src="${imgSrc}" alt="Menu Image">`)}
-</div>
+    <h1 class="page-title">Menu Kami</h1>
+    <div class="image-gallery">
+      ${menuImages
+        .map(
+          (imgSrc) => `
+      <img src="${imgSrc}" alt="Menu item">
+      `,
+        )
+        .join("")}
+    </div>
   `;
+
   container.appendChild(menuDiv);
 }
-
-// export function loadMenu(container) {
-//   console.log("button Menu di klik");
-// }
